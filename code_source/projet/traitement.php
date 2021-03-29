3da2a7734ee2a000ceeaf457e412a9ad73abaae7
CHAHBAR-SOUFIANE <chahbar.soufiane.dev@gmail.com>
2021-03-29 11:26:31 +0100

    Amélioration de code

--- code_source/projet/traitement.php
+++ code_source/projet/traitement.php
@@ -10,24 +10,48 @@ session_start();
  if( isset( $_SESSION['liste_nomS'] ) ) {
     $liste_nom =  $_SESSION['liste_nomS'] ;
  }else {
-   @ $_SESSION['liste_nomS'] =  $liste_nom;
+   $_SESSION['liste_nomS'] =  $liste_nom;
  }
 //prenom
 if( isset( $_SESSION['liste_prenomS'] ) ) {
     $liste_prenom =  $_SESSION['liste_prenomS'] ;
  }else {
-   @ $_SESSION['liste_prenomS'] =  $liste_prenom;
+   $_SESSION['liste_prenomS'] =  $liste_prenom;
+ }
+ //Adress
+if( isset( $_SESSION['liste_adressS'] ) ) {
+    $liste_adress =  $_SESSION['liste_adressS'] ;
+ }else {
+   $_SESSION['liste_adressS'] =  $liste_adress;
+ }
+  //Telephone
+if( isset( $_SESSION['liste_teleS'] ) ) {
+    $liste_tele =  $_SESSION['liste_teleS'] ;
+ }else {
+   $_SESSION['liste_teleS'] =  $liste_tele;
+ }
+   //Email
+if( isset( $_SESSION['liste_emailS'] ) ) {
+    $liste_email =  $_SESSION['liste_emailS'] ;
+ }else {
+   $_SESSION['liste_emailS'] =  $liste_email;
  }
 
 
   // Ajouter le nom et email dans le tableau
- @ $liste_nom[] = $_POST["nom"];
- @ $liste_prenom[] = $_POST["prenom"];
+$liste_nom[] = $_POST["nom"];
+$liste_prenom[] = $_POST["prenom"];
+$liste_adress[] = $_POST["adress"];
+$liste_tele[] = $_POST["tele"];
+$liste_email[] = $_POST["email"];
  
 
   // Enregistrer le tableau dans la session
 $_SESSION['liste_nomS'] =  $liste_nom;
 $_SESSION['liste_prenomS'] =  $liste_prenom;
+$_SESSION['liste_adressS'] =  $liste_adress;
+$_SESSION['liste_teleS'] =  $liste_tele;
+$_SESSION['liste_emailS'] =  $liste_email;
 
 ?>
 <!doctype html>
@@ -51,42 +75,68 @@ td, th {
   padding: 8px;
 }
 
+th{
+    background-color:white;
+}
+
 tr:nth-child(even) {
   background-color: #dddddd;
+}
+
+h2{
+    font-size:2rem;
+}
+
+@media print{
+    h2{
+        display:none;
+    }
+
+}
+
+input{
+    margin:2rem 2rem 2rem 0;
 }
 </style>
 </head>
 <body>
 
-<h1>la listes des participants</h1>
+<h2>list des participants : </h2>
 
 <table border="2">
         <thead>
             <tr>
                 <th>nom</th>
                 <th>prenom</th>
+                <th>Adress</th>
+                <th>Telephone</th>
+                <th>Email</th>
             </tr>
         </thead>
         <tbody>
         <?php 
+        //affichage des participant
         for ($i=0; $i <sizeof($liste_nom); $i++){
      
         echo "<tr>";
         echo "<td>".$liste_nom[$i]."</td>";
         echo "<td>".$liste_prenom[$i]."</td>";
-        echo "</tr>";
+        echo "<td>".$liste_adress[$i]."</td>";
+        echo "<td>".$liste_tele[$i]."</td>";
+        echo "<td>".$liste_email[$i]."</td>";
+        echo "<tr>";
         
         }
+
     ?>  
         </tbody>
     </table>
-
-
+    <input id="print" type="button" value="Imprimer" onclick="window.print()">
    
-
-
-
 </body>
 
 </html>
 
+<?php
+    
+ ?>
