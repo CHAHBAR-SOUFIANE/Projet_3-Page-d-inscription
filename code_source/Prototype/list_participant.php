7ed4134ff5c4dc17146d10231e36e93e7e72aa75
CHAHBAR-SOUFIANE <chahbar.soufiane.dev@gmail.com>
2021-04-09 05:51:35 +0200

    Il y a des erreurs bizarre

---
+++ code_source/Prototype/list_participant.php
@@ -0,0 +1,12 @@
+<?php
+require("participants.php");
+$participantList = [];
+$participant = new participants();
+$participant -> nom = "Ahmad";
+$participant -> prenom = "Alami";
+$participantList = $participant;
+
+$participantList_json =  json_encode($participantList);
+?>
+
+
